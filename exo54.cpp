f2457cabe06e5c227904cc10d1aac306cfa1a761
WPJ008 <wawitephilemon@gmail.com>
2025-07-19 11:42:42 +0200

    exo54

---
+++ exo54.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+//54. Longueur d'une chaîne : Calculer la longueur d'une chaîne de caractères.
+
+int main(){
+	
+	
+	string saisi, result;
+	
+	cout<<"Saisir une chaine : ";
+	
+	getline(cin, saisi);
+	
+   cout<<"La longueur de la chaine : "<< saisi.length();
+   
+   
+   	
+	return 0;
+}
