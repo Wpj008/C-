4e0c606a309d72f45873ea6eec6e55f3d817ad3d
WPJ008 <wawitephilemon@gmail.com>
2025-07-24 12:19:31 +0200

    exo59

---
+++ exo59.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+//59. Comptage des consonnes : Compter le nombre de consonnes dans une chaîne.
+
+int main(){
+	
+	string saisi;
+	char value;
+	int result = 0;
+	
+	cout<<"Saisir une chaine : ";
+	getline(cin, saisi);
+	
+	for(int i = 0; i < saisi.length() ; i++){
+		
+		value = tolower(saisi[i]);
+		
+		if(value != 'a' || value != 'i' ||  value != 'o' ||  value != 'u' ||  value != 'e') {
+			
+			result = result + 1;
+		}
+		
+	}
+	
+	cout<<"Nombre de voyelle dans la chaine : "<<result;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
